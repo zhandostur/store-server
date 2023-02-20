430736e6bd2e80ad4b47fcd03c8c6bc1938377f8
zhandos <tursynbaevzhandos@gmail.com>
2023-02-20 12:35:32 +0600

    Initial commit

---
+++ store/products/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from products.models import ProductCategory, Product, Basket
+
+admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'category']
+    fields = ['name', 'description', ('price', 'quantity'), 'image', 'category']
+    search_fields = ['name']
+    ordering = ['name']
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ['product', 'quantity']
+    extra = 0
---
+++ store/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from users.views import registration, login, profile, logout
+
+app_name = 'users'
+urlpatterns = [
+    path('registration/', registration, name='registration'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
+]
